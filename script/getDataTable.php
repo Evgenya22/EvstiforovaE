3be6e4d98cceed91f9cddd5bab51d840a2549b48
Evgenya <eeyu.21@uni-dubna.ru>
2023-01-09 21:51:00 +0300

    12

--- script/getDataTable.php
+++ script/getDataTable.php
@@ -35,12 +35,19 @@ $sql = "SELECT * FROM task1";
 if($result = $conn->query($sql)){
     $rowsCount = $result->num_rows; // количество полученных строк
     echo "<p>Получено объектов: $rowsCount</p>";
-    echo "<table><tr><th>Id</th><th>Имя</th><th>Возраст</th></tr>";
+    echo "<table><tr><th>Id</th><th>ФИО</th><th>Дата рождения</th><th>Телефон</th><th>Электронная почта</th><th>Имеется ли доклад</th>
+    <th>Тема доклада</th><th>Секция конференции</th></tr>";
     foreach($result as $row){
         echo "<tr>";
             echo "<td>" . $row["id"] . "</td>";
             echo "<td>" . $row["fio"] . "</td>";
+            echo "<td>" . $row["BirthDate"] . "</td>";
             echo "<td>" . $row["phone"] . "</td>";
+            echo "<td>" . $row["email"] . "</td>";
+            echo "<td>" . $row["theme"] . "</td>";
+            echo "<td>" . $row["conference"] . "</td>";
+
+
         echo "</tr>";
     }
     echo "</table>";
